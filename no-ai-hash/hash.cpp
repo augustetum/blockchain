776c3e0db95c3533e7b622ae407f7e2df3ad6aa5
augustetum <auguste.tumaite@gmail.com>
2025-09-24 12:43:30 +0300

    Functions, file input

---
+++ no-ai-hash/hash.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include "functions.h"
+
+int main(){
+
+    int menuChoice;
+                
+    std::cout << "Pasirinkite programos eigą:" << std::endl;
+    std::cout << "1 | Įvesti norimą hashuoti tekstą ranka" << std::endl;
+    std::cout << "2 | Nuskaityti failą" << std::endl;
+    std::cin >> menuChoice;
+
+    switch(menuChoice){
+        case 1: {
+            std::cout << "Pasirinkote teksto įvestį ranka" << std::endl;
+            std::cout << "---------------------------------" << std::endl;
+
+            std::string hashuojamasTekstas;
+            std::cin >> hashuojamasTekstas;
+            break;
+        }  
+
+        case 2: {
+            std::cout << "Pasirinkote nuskaityti duomenis iš failo" << std::endl;
+            std::cout << "----------------------------------------" << std::endl;
+
+            try {
+                std::string fail = pasirinktiFaila();
+            } catch (const char* e){
+                std::cout << "Nuskaitymas nepavyko. " << e << std::endl;
+                break;
+            } catch (const std::runtime_error &e){
+                std::cout << e.what();
+            }
+
+            break;
+
+        }
+    }
+
+
+
+    
+
+
+
+    return 0;
+}
